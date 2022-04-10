5ade933a59036ee2d090665ee20b5ec5cd463b7b
Bash Elliott <spicethings9@gmail.com>
2022-04-11 01:16:19 +1000

    wrote readme

---
+++ examples/basic.py
@@ -0,0 +1,7 @@
+from bashtable import bashtable
+
+table = bashtable()
+table.setData(0, "Bash", "Elliott", "17")
+table.setData(1, "Jane", "Doe", "21")
+table.setData(2, "John", "Doe", "21")
+table.draw()
